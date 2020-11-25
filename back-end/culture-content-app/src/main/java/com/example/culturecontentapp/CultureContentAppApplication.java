68cc54ddbb0cb86fcaf8bc2d83ce53be59be266f
Petar Cerovic <quadrition@gmail.com>
2020-11-25 11:22:30 +0100

    Initialized empty spring project

---
+++ back-end/culture-content-app/src/main/java/com/example/culturecontentapp/CultureContentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.culturecontentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureContentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureContentAppApplication.class, args);
+	}
+
+}
