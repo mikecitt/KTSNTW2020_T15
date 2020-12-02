5538e303afd417e9795e2ccb6360987f7e617e61
Petar Cerovic <quadrition@gmail.com>
2020-12-02 22:23:46 +0100

    Added funcitonality for adding a new cultural offer

--- back-end/culture-content-app/src/main/java/com/example/culturecontentapp/exception/ControllerExceptionHandler.java
+++ back-end/culture-content-app/src/main/java/com/example/culturecontentapp/exception/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -27,32 +28,34 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 
-
   @ExceptionHandler
-  protected ResponseEntity<Object> handleTypeNotFoundException(TypeNotFoundException ex,
-       WebRequest request){
+  protected ResponseEntity<Object> handleTypeNotFoundException(TypeNotFoundException ex, WebRequest request) {
 
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler
-  protected ResponseEntity<Object> handleTypeAlreadyExistsException(TypeAlreadyExistsException ex,
-                                                                    WebRequest request){
+  protected ResponseEntity<Object> handleTypeAlreadyExistsException(TypeAlreadyExistsException ex, WebRequest request) {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
   @ExceptionHandler
-  protected ResponseEntity<Object> handleSubTypeNotFoundException(SubTypeNotFoundException ex,
-                                                                  WebRequest request){
+  protected ResponseEntity<Object> handleSubTypeNotFoundException(SubTypeNotFoundException ex, WebRequest request) {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler
   protected ResponseEntity<Object> handleSubTypeAlreadyExistsException(SubTypeAlreadyExistsException ex,
-                                                                       WebRequest request){
+      WebRequest request) {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
   }
-  
+
+  @ExceptionHandler(CulturalOfferAlreadyExistsException.class)
+  protected ResponseEntity<Object> handleCulturalOfferAlreadyExistsException(CulturalOfferAlreadyExistsException ex,
+      WebRequest request) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
   @ExceptionHandler(StorageException.class)
   protected ResponseEntity<Object> handleStorageException(StorageException ex, WebRequest request) {
 
@@ -64,5 +67,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
       HttpStatus status, WebRequest request) {
 
     return new ResponseEntity<>(ex.getBindingResult().getAllErrors(), HttpStatus.BAD_REQUEST);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 }
