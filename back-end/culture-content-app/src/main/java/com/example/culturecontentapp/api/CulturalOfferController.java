dcfd6a5d4b1cc0dba525279b25b7617a28e069a8
Petar Cerovic <quadrition@gmail.com>
2020-12-07 19:17:13 +0100

    Added functionality for deleting a cultural offer

--- back-end/culture-content-app/src/main/java/com/example/culturecontentapp/api/CulturalOfferController.java
+++ back-end/culture-content-app/src/main/java/com/example/culturecontentapp/api/CulturalOfferController.java
@@ -5,9 +5,12 @@ import javax.validation.Valid;
 import com.example.culturecontentapp.payload.request.NewCulturalOfferRequest;
 import com.example.culturecontentapp.payload.response.EditCulturalOfferResponse;
 import com.example.culturecontentapp.payload.response.NewCulturalOfferResponse;
+import com.example.culturecontentapp.payload.response.SelectCulturalOfferResponse;
 import com.example.culturecontentapp.service.CulturalOfferService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/cultural-offer")
@@ -45,5 +49,15 @@ public class CulturalOfferController {
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     return service.delete(id);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<SelectCulturalOfferResponse>> select(Pageable pageable) {
+    return service.select(pageable);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<SelectCulturalOfferResponse> selectById(@PathVariable Long id) {
+    return service.selectById(id);
   }
 }
--- back-end/culture-content-app/src/main/java/com/example/culturecontentapp/repository/CulturalOfferRepository.java
+++ back-end/culture-content-app/src/main/java/com/example/culturecontentapp/repository/CulturalOfferRepository.java
@@ -4,11 +4,15 @@ import java.util.Optional;
 
 import com.example.culturecontentapp.model.CulturalOffer;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CulturalOfferRepository extends JpaRepository<CulturalOffer, Long> {
+
+  Page<CulturalOffer> findAll(Pageable pageable);
 
   Optional<CulturalOffer> findByName(String name);
 }
