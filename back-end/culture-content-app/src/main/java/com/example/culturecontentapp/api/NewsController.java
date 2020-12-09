9f2a54ecf53597bf6061bd0646ff7a2c2f7eb357
Vukasin <vukasinj98@gmail.com>
2020-12-09 17:57:58 +0100

    Changed request mapping for news

--- back-end/culture-content-app/src/main/java/com/example/culturecontentapp/api/NewsController.java
+++ back-end/culture-content-app/src/main/java/com/example/culturecontentapp/api/NewsController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/news")
+@RequestMapping("api/news")
 @RestController
 public class NewsController {
 
