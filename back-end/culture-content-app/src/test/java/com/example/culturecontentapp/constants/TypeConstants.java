ba5661a5b004b6f7f002b7ba488434d0c402468b
Nikola <nstojanovic998@gmail.com>
2020-12-19 13:40:38 +0100

    testFIndByName

---
+++ back-end/culture-content-app/src/test/java/com/example/culturecontentapp/constants/TypeConstants.java
@@ -0,0 +1,11 @@
+package com.example.culturecontentapp.constants;
+
+public class TypeConstants {
+    public static final String NEW_TYPE = "Kulturno dobro";
+
+    public static final String DB_TYPE = "Manifestacija";
+    public static final Long DB_TYPE_ID = 1001L;
+
+    public static final long FIND_ALL_NUMBER_OF_ITEMS = 1;
+
+}
