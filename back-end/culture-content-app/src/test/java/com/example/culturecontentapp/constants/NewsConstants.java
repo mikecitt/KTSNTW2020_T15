9157b0f4ffe67dc96734fdb58c9228fddf3d33a7
Vukasin <vukasinj98@gmail.com>
2020-12-21 13:32:41 +0100

    Tested news repository

---
+++ back-end/culture-content-app/src/test/java/com/example/culturecontentapp/constants/NewsConstants.java
@@ -0,0 +1,14 @@
+package com.example.culturecontentapp.constants;
+
+public class NewsConstants {
+
+    public static final Long NEWS_ID = 1001l;
+    public static final Long CATEGORY_ID = 1001l;
+
+    public static final Integer PAGEABLE_PAGE = 0;
+    public static final Integer PAGEABLE_SIZE = 1;
+    public static final Integer PAGEABLE_TOTAL_ELEMENTS = 1;
+
+    public static final long DB_NEWS_SIZE = 1;
+    
+}
---
+++ back-end/culture-content-app/src/test/java/com/example/culturecontentapp/repository/NewsRepositoryIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.example.culturecontentapp.repository;
+
+import static com.example.culturecontentapp.constants.NewsConstants.CATEGORY_ID;
+import static com.example.culturecontentapp.constants.NewsConstants.DB_NEWS_SIZE;
+import static com.example.culturecontentapp.constants.NewsConstants.PAGEABLE_PAGE;
+import static com.example.culturecontentapp.constants.NewsConstants.PAGEABLE_SIZE;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import com.example.culturecontentapp.model.News;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class NewsRepositoryIntegrationTest {
+
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @Test
+    public void testFindByCulturalOffer() {
+        Pageable pageable = PageRequest.of(PAGEABLE_PAGE,PAGEABLE_SIZE);
+        List<News> found = newsRepository.findByCulturalOffer(CATEGORY_ID,pageable);
+        assertEquals(DB_NEWS_SIZE, found.size());
+    }
+    
+}
