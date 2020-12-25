279a61df2616deb75014d3dffc5673117f078d5b
Nikola <nstojanovic998@gmail.com>
2020-12-25 17:01:30 +0100

    Added search and filter functionality

--- back-end/culture-content-app/src/main/resources/data-postgres.sql
+++ back-end/culture-content-app/src/main/resources/data-postgres.sql
@@ -9,8 +9,17 @@ INSERT INTO Type (id, name) VALUES
 INSERT INTO Sub_type (id, name, type_id) VALUES
     (1001, 'Festival', 1001);
 
+INSERT INTO Sub_type (id, name, type_id) VALUES
+    (1002, 'Koncert', 1001);
+
 INSERT INTO Cultural_offer (id, description, location, name, subtype_id) VALUES
     (1001, 'Najveci festival u regionu', 'Petrovaradinska tvrdjava', 'Exit festival', 1001);
+
+INSERT INTO Cultural_offer (id, description, location, name, subtype_id) VALUES
+    (1002, 'Najveci festival u regionu', 'Usce', 'BeerFest', 1001);
+
+INSERT INTO Cultural_offer (id, description, location, name, subtype_id) VALUES
+    (1003, 'Najveci festival u regionu', 'Petrovaradinska tvrdjava', 'Koncert Zdravko colic', 1002);
 
 INSERT INTO Review (id, comment, rating, user_id, culturaloffer_id) VALUES
     (1001, 'extra', 5, 1001, 1001);
