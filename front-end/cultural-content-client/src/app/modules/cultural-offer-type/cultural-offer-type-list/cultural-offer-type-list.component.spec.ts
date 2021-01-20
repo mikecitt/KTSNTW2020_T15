64c96034aacae8093cb8bc515c66530fb9a98599
Nikola <nstojanovic998@gmail.com>
2021-01-20 16:11:16 +0100

    Added tests for forms and table

--- front-end/cultural-content-client/src/app/modules/cultural-offer-type/cultural-offer-type-list/cultural-offer-type-list.component.spec.ts
+++ front-end/cultural-content-client/src/app/modules/cultural-offer-type/cultural-offer-type-list/cultural-offer-type-list.component.spec.ts
@@ -22,4 +22,40 @@ describe('CulturalOfferTypeListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should assign type list to dataSource', async() => {
+    component.ngOnInit();
+    expect(component.dataSource).toBeDefined();
+  });
+
+  it('should assign type list to dataSource on changes', async() => {
+    component.ngOnChanges();
+    expect(component.dataSource).toBeDefined();
+  });
+
+  it('should emit on click create', async () =>{
+    spyOn(component.onCreateTypeEvent, 'emit');
+    component.createType();
+    fixture.detectChanges();
+    expect(component.onCreateTypeEvent.emit).toHaveBeenCalled();
+    expect(component.onCreateTypeEvent.emit).toHaveBeenCalledWith('create');
+  });
+
+  it('should emit on click delete', async () => {
+    spyOn(component.onDeleteTypeEvent,'emit');
+    component.deleteType({id: 1});
+    fixture.detectChanges();
+    expect(component.onDeleteTypeEvent.emit).toHaveBeenCalled();
+    expect(component.onDeleteTypeEvent.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should emit on click updateType', async () => {
+    spyOn(component.onUpdateTypeEvent,'emit');
+    component.updateType({id:1, name:"tip"});
+    fixture.detectChanges();
+    expect(component.onUpdateTypeEvent.emit).toHaveBeenCalled();
+    expect(component.onUpdateTypeEvent.emit).toHaveBeenCalledWith({id:1, name:"tip"});
+  });
+
+
 });
