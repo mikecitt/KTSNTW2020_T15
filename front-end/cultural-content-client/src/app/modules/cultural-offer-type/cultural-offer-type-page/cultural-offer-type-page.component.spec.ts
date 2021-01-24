7f69c144e4e106b66977b40c26505587136f062c
Vukasin <vukasinj98@gmail.com>
2021-01-24 14:47:36 +0100

    Tests for subtype on front-end done

--- front-end/cultural-content-client/src/app/modules/cultural-offer-type/cultural-offer-type-page/cultural-offer-type-page.component.spec.ts
+++ front-end/cultural-content-client/src/app/modules/cultural-offer-type/cultural-offer-type-page/cultural-offer-type-page.component.spec.ts
@@ -1,3 +1,5 @@
+import { SnackBarComponent } from './../../../core/snack-bar/snack-bar.component';
+import { CulturalOfferSubTypeService } from './../../../services/cultural-offer-subtype/cultural-offer-sub-type.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Observable, of } from 'rxjs';
@@ -13,6 +15,8 @@ describe('CulturalOfferTypePageComponent', () => {
   let fixture: ComponentFixture<CulturalOfferTypePageComponent>;
   let typeService: any;
   let dialog: any;
+  let subtypeService: any;
+  let snackbar: SnackBarComponent;
 
   beforeEach(async () => {
     const typesMock = {
@@ -24,9 +28,18 @@ describe('CulturalOfferTypePageComponent', () => {
       totalElements: 3
     }
 
+    const subTypesMock = {
+      content: [
+        {id: 1, name: "podtip1"},
+        {id:2, name: "podtip2"},
+        {id: 3, name: "podtip3"}
+      ],
+      totalElements: 3
+    }
+
     let typeServiceMock = {
       getAllPaginated : jasmine.createSpy('getAllPaginated')
-                         .and.returnValue(of(typesMock)),
+                         .and.returnValue(of(subTypesMock)),
       deleteType: jasmine.createSpy('deleteType')
                          .and.returnValue(of()),
       createType: jasmine.createSpy('createType')
@@ -34,17 +47,29 @@ describe('CulturalOfferTypePageComponent', () => {
       updateType: jasmine.createSpy('updateType')
                          .and.returnValue(of({}))
     };
+
+    let subTypeServiceMock = {
+      getAllPaginated : jasmine.createSpy('getAllPaginated')
+                         .and.returnValue(of(typesMock))
+    };
+
     let dialogMock = {
       open : jasmine.createSpy('open').and.returnValue({
          afterClosed : jasmine.createSpy('afterClosed').and.returnValue( of({}) ), close: null
         })
     };
 
+    const snackbarMock = {
+      openSnackBar: jasmine.createSpy('openSnackBar')
+    }
+
     await TestBed.configureTestingModule({
       declarations: [ CulturalOfferTypePageComponent ],
       providers : [
         {provide: CulturalOfferTypeService, useValue: typeServiceMock},
-        {provide: MatDialog, useValue: dialogMock}
+        {provide: MatDialog, useValue: dialogMock},
+        {provide: CulturalOfferSubTypeService, useValue: subTypeServiceMock},
+        {provide: SnackBarComponent, useValue: snackbarMock}
       ]
     });
     //.compileComponents();
@@ -55,7 +80,9 @@ describe('CulturalOfferTypePageComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     typeService = TestBed.inject(CulturalOfferTypeService);
+    subtypeService = TestBed.inject(CulturalOfferSubTypeService);
     dialog = TestBed.inject(MatDialog);
+    snackbar = TestBed.inject(SnackBarComponent);
   });
 
   it('should fetch the type list on init', async () => {
@@ -72,7 +99,7 @@ describe('CulturalOfferTypePageComponent', () => {
            });
   });
 
-  it('should go to next page in table', async () => {
+  it('should go to next page in type table', async () => {
     component.getNextType();
 
     expect(component.curentPageType).toBe(1);
@@ -88,7 +115,7 @@ describe('CulturalOfferTypePageComponent', () => {
       })
   });
 
-  it('should go to previous page in table', async () => {
+  it('should go to previous page in type table', async () => {
     component.curentPageType = 1;
     component.getPreviousType();
 
@@ -103,6 +130,39 @@ describe('CulturalOfferTypePageComponent', () => {
         //  let elements: DebugElement[] = fixture.debugElement.queryAll(By.css('tr'));
         expect(elements.length).toBe(4);
       })
+  });
+
+  it('should go to next page in subtype table', async () => {
+    component.getNextSubType();
+
+    expect(component.curentPageSubType).toBe(1);
+    expect(subtypeService.getAllPaginated).toHaveBeenCalled();
+
+    // fixture.whenStable()
+    //   .then( () => {
+    //     expect(component.culturalOfferSubTypes.length).toBe(3);
+    //     fixture.detectChanges();
+    //     let elements: DebugElement[] = fixture.debugElement.query(el => el.name === 'tr').nativeElement;  // it works
+    //     // let elements: DebugElement[] = fixture.debugElement.queryAll(By.css('tr'));
+    //     expect(elements.length).toBe(4);
+    //   })
+  });
+
+  it('should go to previous page in subtype table', async () => {
+    component.curentPageSubType = 1;
+    component.getPreviousSubType();
+
+    expect(component.curentPageSubType).toBe(0);
+    expect(subtypeService.getAllPaginated).toHaveBeenCalled();
+
+    // fixture.whenStable()
+    //   .then( () => {
+    //     expect(component.culturalOfferSubTypes.length).toBe(3);
+    //     fixture.detectChanges();
+    //     let elements: DebugElement[] = fixture.debugElement.query(el => el.name === 'tr').nativeElement;  // it works
+    //     //  let elements: DebugElement[] = fixture.debugElement.queryAll(By.css('tr'));
+    //     expect(elements.length).toBe(4);
+    //   })
   });
 
   it('should call delete type', async () => {
