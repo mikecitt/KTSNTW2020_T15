bec83095955b55e632ffab29ffb2641dd463f40c
Mikec <mikecitt@gmail.com>
2021-01-26 13:57:39 +0100

    Added component for displaying all reviews for each cultural offer.

---
+++ front-end/cultural-content-client/src/app/services/reviews/review.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ReviewPage } from 'src/app/models/review-page';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  constructor(private http:HttpClient) { }
+
+  getAll(culturalOfferId: number, page: number, limit: number): Observable<ReviewPage>{
+    const params: HttpParams = new HttpParams()
+        .append('page', page.toString())
+        .append('size', limit.toString());
+    return this.http.get<ReviewPage>(environment.api_url + '/review?culturalOfferId=' + culturalOfferId, {params : params});
+  }
+}
