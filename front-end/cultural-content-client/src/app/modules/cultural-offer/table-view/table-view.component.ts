d16d80ece5700bc42433ed3cfeaeef82c227e1eb
Petar Cerovic <quadrition@gmail.com>
2021-01-27 09:04:49 +0100

    Added table view

---
+++ front-end/cultural-content-client/src/app/modules/cultural-offer/table-view/table-view.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { CulturalOfferResponse } from 'src/app/models/cultural-offer-response';
+import { CulturalOfferService } from 'src/app/services';
+
+@Component({
+  selector: 'app-table-view',
+  templateUrl: './table-view.component.html',
+  styleUrls: ['./table-view.component.scss'],
+})
+export class TableViewComponent implements AfterViewInit, OnChanges {
+  displayedColumns: string[] = ['name', 'location', 'subType'];
+
+  @Input()
+  culturalOffers: CulturalOfferResponse[];
+
+  dataSource = new MatTableDataSource<CulturalOfferResponse>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private service: CulturalOfferService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['culturalOffers']) {
+      this.dataSource.data = this.culturalOffers;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.service.getLocations().subscribe((data) => {
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+}
