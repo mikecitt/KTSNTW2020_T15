1e80a6642271d0c53fd99baacf3ba56d77aebb11
Vukasin <vukasinj98@gmail.com>
2021-01-27 20:11:15 +0100

    Guard test done

--- front-end/cultural-content-client/src/app/guards/admin.guard.spec.ts
+++ front-end/cultural-content-client/src/app/guards/admin.guard.spec.ts
@@ -1,16 +1,57 @@
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 
 import { AdminGuard } from './admin.guard';
+import { UserService } from '../services';
 
 describe('AdminGuard', () => {
   let guard: AdminGuard;
+  let router: Router;
+  let userService: UserService;
+
+  const routerMock = jasmine.createSpyObj('router', ['navigate']);
+
+  const serviceMock = {
+    currentUser : {},
+    getRole : jasmine.createSpy('getRole')
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AdminGuard);
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: serviceMock}, 
+        {provide: Router, useValue: routerMock}]
+    });
+    
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should not activate', async() => {   
+    userService = TestBed.inject(UserService);
+    guard = TestBed.inject(AdminGuard);
+    router = TestBed.inject(Router);
+    const val = guard.canActivate(({data: { expectedRoles: 'ROLE_ADMIN'}} as any) as ActivatedRouteSnapshot, ({url: ""} as any) as RouterStateSnapshot);
+
+    expect(val).toBeFalsy();
+    expect(userService.getRole).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalled();
+  });
+
+  it('should activate', async() => {
+    const serviceMock2 = {
+      currentUser: {},
+      getRole : jasmine.createSpy('getRole').and.returnValue('ROLE_ADMIN')
+    }
+    TestBed.overrideProvider(UserService, {useValue: serviceMock2});
+    userService = TestBed.inject(UserService);
+    guard = TestBed.inject(AdminGuard);
+    router = TestBed.inject(Router);
+    const val = guard.canActivate(({data: { expectedRoles: 'ROLE_ADMIN'}} as any) as ActivatedRouteSnapshot, ({url: ""} as any) as RouterStateSnapshot);
+
+    expect(val).toBeTruthy();
+    expect(userService.getRole).toHaveBeenCalled();
   });
 });
--- front-end/cultural-content-client/src/app/guards/user.guard.spec.ts
+++ front-end/cultural-content-client/src/app/guards/user.guard.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { UserService } from '../services';
 
 import { UserGuard } from './user.guard';
 
 describe('UserGuard', () => {
   let guard: UserGuard;
+  let router: Router;
+  let userService: UserService;
+
+  const routerMock = jasmine.createSpyObj('router', ['navigate']);
+
+  const serviceMock = {
+    currentUser : {},
+    getRole : jasmine.createSpy('getRole')
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(UserGuard);
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: serviceMock}, 
+        {provide: Router, useValue: routerMock}]
+    });
   });
 
-  it('should be created', () => {
+  it('should be created', () => {    
+    userService = TestBed.inject(UserService);
+    guard = TestBed.inject(UserGuard);
+    router = TestBed.inject(Router);
     expect(guard).toBeTruthy();
+  });
+
+  it('should not activate', async() => {   
+    userService = TestBed.inject(UserService);
+    guard = TestBed.inject(UserGuard);
+    router = TestBed.inject(Router);
+    const val = guard.canActivate(({data: { expectedRoles: 'ROLE_USER'}} as any) as ActivatedRouteSnapshot, ({url: ""} as any) as RouterStateSnapshot);
+
+    expect(val).toBeFalsy();
+    expect(userService.getRole).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalled();
+  });
+
+  it('should activate', async() => {
+    const serviceMock2 = {
+      currentUser: {},
+      getRole : jasmine.createSpy('getRole').and.returnValue('ROLE_USER')
+    }
+    TestBed.overrideProvider(UserService, {useValue: serviceMock2});
+    userService = TestBed.inject(UserService);
+    guard = TestBed.inject(UserGuard);
+    router = TestBed.inject(Router);
+    const val = guard.canActivate(({data: { expectedRoles: 'ROLE_USER'}} as any) as ActivatedRouteSnapshot, ({url: ""} as any) as RouterStateSnapshot);
+
+    expect(val).toBeTruthy();
+    expect(userService.getRole).toHaveBeenCalled();
   });
 });
